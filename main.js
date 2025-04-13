c5f630dd9f9460007de45fd8a9c85b0c5e459466
zoirovdev <abboszoirov17@gmail.com>
2025-04-13 00:47:53 +0000

    connect server

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
+const btnEl = document.getElementById("btn");
 
+
+async function getData(){
+	try {
+		const response = await fetch("https://qapi.vercel.app/api/random");
+		const data = await response.json();
+		quoteEl.innerText = data.quote;
+		authorEl.innerText = data.author;
+	} catch (error) {
+		console.error("error fetching data: ", error);
+	}
+}
+
+
+btnEl.addEventListener("click", getData);
